b5f4ebe940d844709f07db7ef7661f41663d3a08
dkilav <vlad5222vd@gmail.com>
2023-09-29 13:03:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
